61c9368903778f0dbd68937df844b8f12337ed1a
reza hajitabar <rezahajitabarchehre1996@gmail.com>
2025-05-19 23:55:58 -0700

    eleven commit

--- src/components/modules/Chart.jsx
+++ src/components/modules/Chart.jsx
@@ -1,46 +1,74 @@
-import React, { useState } from 'react';
-import styles from '../modules/Chart.module.css';
-import {convertData} from "../helper/convertData";
-import { CartesianGrid, LineChart, ResponsiveContainer,Line, YAxis, XAxis, Legend, Tooltip } from 'recharts';
-function Chart({chart,setChart}) {
+import React, { useState } from "react";
+import styles from "../modules/Chart.module.css";
+import { convertData } from "../helper/convertData";
+import {
+  CartesianGrid,
+  LineChart,
+  ResponsiveContainer,
+  Line,
+  YAxis,
+  XAxis,
+  Legend,
+  Tooltip,
+} from "recharts";
+function Chart({ chart, setChart }) {
+  const [type, setType] = useState("prices");
+  console.log(convertData(chart, type));
 
-  const [type,setType]=useState("prices")
-  console.log(convertData(chart,type));
-  
   return (
     <div className={styles.container}>
-      <span className={styles.cross} onClick={()=> setChart(null)}>X</span>
+      <span className={styles.cross} onClick={() => setChart(null)}>
+        X
+      </span>
       <div className={styles.chart}>
         <div className={styles.name}>
           <img src={chart.coin.image} />
           <p>{chart.coin.name}</p>
         </div>
-         <div className={styles.graph}>
-         <ChartComponent data={convertData(chart,type)} type={type}/>
-         </div>
-         <div className={styles.types}>
-         <button onClick={(e)=>setType(e.target.innerText)}>prices</button>
-         <button onClick={(e)=>setType(e.target.innerText)}>market_caps</button>
-         <button onClick={(e)=>setType(e.target.innerText)}>total_volume</button>
-         </div>
-      </div>  
+        <div className={styles.graph}>
+          <ChartComponent data={convertData(chart, type)} type={type} />
+        </div>
+        <div className={styles.types}>
+          <button onClick={(e) => setType(e.target.innerText)}>prices</button>
+          <button onClick={(e) => setType(e.target.innerText)}>
+            market_caps
+          </button>
+          <button onClick={(e) => setType(e.target.innerText)}>
+            total_volume
+          </button>
+        </div>
+        <div className={styles.details}>
+          <div>
+            <p>Prices :</p>
+            <span>${chart.coin.current_price}</span>
+          </div>
+          <div>
+            <p>ATH :</p>
+            <span>${chart.coin.ath}</span>
+          </div>
+          <div>
+            <p>Market Cap :</p>
+            <span>${chart.coin.market_cap}</span>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Chart
+export default Chart;
 
-const ChartComponent=({data,type})=>{
+const ChartComponent = ({ data, type }) => {
   return (
     <ResponsiveContainer width="100%" height="100%">
-    <LineChart width={400} height={400} data={data}>
-    <Line type="monotone" dataKey={type} stroke="#3874ff" strokeWidth={2}/>
-      <CartesianGrid stroke="#404042"/>
-      <YAxis dataKey={type} domain={["auto","auto"]}/>
-      <XAxis dataKey="date"/>
-      <Legend/>
-      <Tooltip/>
-    </LineChart>
-   </ResponsiveContainer>
-  )
-}
+      <LineChart width={400} height={400} data={data}>
+        <Line type="monotone" dataKey={type} stroke="#3874ff" strokeWidth={2} />
+        <CartesianGrid stroke="#404042" />
+        <YAxis dataKey={type} domain={["auto", "auto"]} />
+        <XAxis dataKey="date" />
+        <Legend />
+        <Tooltip />
+      </LineChart>
+    </ResponsiveContainer>
+  );
+};
